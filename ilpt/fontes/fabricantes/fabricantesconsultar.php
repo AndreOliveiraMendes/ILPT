6e3f2d8535ec8071f8577976ec428011fd924e44
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-11-10 15:33:07 -0300

    Update fabricantesconsultar.php
    formatation

--- ilpt/fontes/fabricantes/fabricantesconsultar.php
+++ ilpt/fontes/fabricantes/fabricantesconsultar.php
@@ -1,35 +1,33 @@
 <?php
 #--------------------------------------------------------------------------------------------------------------------------------
 # Programa....: medicosconsultar (arquivo medicosconsultar.php)
-# DescriÃ§Ã£o...: Este Ã© o conjunto de funÃ§Ãµes. SÃ£o as funÃ§Ãµes que de modo mais comum serÃ£o utilizadas em outros programas.
-#               Todas sÃ£o declaradas como funÃ§Ãµes e passa a ficar "disponÃ­veis" no programa que referenciar o toolkit.php
-# Objetivo....: Declarar e disponibilizar as funÃ§Ãµes mais comuns no sistema.
+# Descrição...: Este eh PA-Consultar. Este PA tem 2 'cases': case 1 monta um formulario mostrando a caixa de escolha. No case 2
+#               exibe os dados detalhados do registro que foi escolhido.
+# Objetivo....: Mostrar uma caixa de selecao e a seguir os detalhes do registro escolhido.
 # Autor.......: JMH - Copie, mas diga que fez!
-# CriaÃ§Ã£o.....: 2018-09-24
-# AtualizaÃ§Ã£o.: 2018-09-24 - CriaÃ§Ã£o e desenvolvimento das funÃ§Ãµes: iniciapagina() e fimdepagina()
-#               2018-10-01 - InclusÃ£o da funÃ§Ã£o de conexÃ£o com o banco de dados MySQL.
-#               2018-10-02 - Comandos para ajuste dos caracteres acentuados em portuguÃªs, nas funÃ§Ãµes: iniciapagina e conecta_my.
-#               2019-10-24 - Adaptando o texto para um comando de printf() para todas as TAGs do iniciapagina()
+# Criação.....: 2019-10-23
+# Atualização.: 2019-10-24 - Estruturacao do PA e desenvolvimento das chamadas das funcoes.
+#               2019-10-24 - Teste de conformidade
 #--------------------------------------------------------------------------------------------------------------------------------
 require_once("../../funcoes/toolkit.php");
 require_once("./fabricantesfuncoes.php");
 iniciapagina("fabricantes","Consulta","#F1F1F1");
-# Este Ã© um exemplo de programa recursivo
-# Usa-se uma das funÃ§Ãµes de ambiente do PHP que
-# Veririfica se exsite valor em variÃ¡vel ou vetor.
-# A funÃ§Ã£o ISSET() retorna True ou False se existe ou nÃ£o valor em
-# uma variÃ¡vel.
+# Este é um exemplo de programa recursivo
+# Usa-se uma das funções de ambiente do PHP que
+# Veririfica se exsite valor em variável ou vetor.
+# A função ISSET() retorna True ou False se existe ou não valor em
+# uma variável.
 $bloco=( ISSET($_POST['bloco']) ) ? $_POST['bloco'] : 1;
-# Separador de Blocos LÃ³gicos do programa
+# Separador de Blocos Lógicos do programa
 switch ($bloco)
 {
   case (1):
-  { # chamada da funÃ§Ã£o que monta a caixa de seleÃ§Ã£o para escolha do registro
+  { # chamada da função que monta a caixa de seleção para escolha do registro
     picklist("fabricantesconsultar.php");
     break;
   }
   case (2):
-  { # Executando a funÃ§Ã£o que mostra os detalhes do registro escolhido no picklist.
+  { # Executando a função que mostra os detalhes do registro escolhido no picklist.
     mostraregistro("$_REQUEST[pkfabricante]");
     break;
   }
