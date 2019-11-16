29f395fabb6e3290d0b258e1cdbb143c80647c51
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-11-15 21:55:47 -0300

    Update gold.c
    2nd version

--- interfatecs/interfatec_2015/InterFatec2015InOut/gold/gold.c
+++ interfatecs/interfatec_2015/InterFatec2015InOut/gold/gold.c
@@ -3,22 +3,17 @@
 #include<math.h>
 int is_prime(int);
 int get_next_prime(int);
-void gold(int, int*, int);
+int* increase_prime(int, int*, int*);
+int* gold(int, int*, int*);
 int main(){
 	int n, qtd = 0;
 	int* prime = NULL;
-	n = 1;
-	do{
-		n = get_next_prime(n);
-		prime = (int*) realloc(prime, (qtd + 1)*sizeof(int));
-		prime[qtd++] = n;
-	} while (n < 1000000);
 	scanf("%d", &n);
 	while(n != 0){
 		if(n%2 == 1)
 			printf("erro\n");
 		else
-			gold(n, prime, qtd);
+			prime = gold(n, prime, &qtd);
 		scanf("%d", &n);
 	}
 	free(prime);
@@ -41,22 +36,42 @@ int get_next_prime(int p){
 	} while (!is_prime(p));
 	return p;
 }
-void gold(int n, int* prime, int qtd){
-	int i, j;
-	for(i = 0; i < qtd; i++){
-		if(prime[i] > n)
-			break;
-		else{
-			for(j = i; j < qtd; j++){
-				if(prime[j] > n || prime[i] + prime[j] == n)
-					break;
-			}
+/*
+	prime[i]+prime[j] <= n
+*/
+int* gold(int n, int* prime, int* qtd){
+	int i = 0, j;
+	prime = increase_prime(0, prime, qtd);
+	while(prime[i] <= n){
+		j = i;
+		while(prime[j] <= n - prime[i]){
 			if(prime[i] + prime[j] == n)
 				break;
+			j++;
+			prime = increase_prime(j, prime, qtd);
 		}
+		if(prime[i] + prime[j] == n)
+			break;
+		i++;
+		prime = increase_prime(i, prime, qtd);
 	}
 	if(prime[i] + prime[j] == n)
 		printf("%d %d\n", prime[i], prime[j]);
 	else
 		printf("do i even need to care about this?\n");
+	return prime;
+}
+/*
+	0	1	2	3	...	qtd - 1
+*/
+int* increase_prime(int i, int* prime, int* qtd){
+	while(i >= *qtd){
+		prime = (int*) realloc(prime, (*qtd + 1)*sizeof(int));
+		if(*qtd == 0)
+			prime[*qtd] = 2;
+		else
+			prime[*qtd] = get_next_prime(prime[*qtd - 1]);
+		*qtd += 1;
+	}
+	return prime;
 }
